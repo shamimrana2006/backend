12272c601af0ce8268a9ab5850695c2a462fadc7
MD Shamim Rana <98444817+shamimrana2006@users.noreply.github.com>
2024-06-07 22:31:57 +0600

    update

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ import express from "express";
 const app = express()
 const port = 3000
 
+import cors from "cors"
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get("/",(req,res)=>{
     res.json({
         name: "shamim",
         roll: 670778
     })
 })
+
 export default app
